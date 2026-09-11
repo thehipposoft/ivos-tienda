66cd1b0041e746e365477c205313192fa420c3ce
Diego Borigen <diego@iddigital.com.au>
2026-09-11 10:13:40 -0300

    Hace locations clickleable

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -11,6 +11,9 @@ const LINK_ICON_SIZE = 28;
 const MAP_EMBED_URL =
   "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.7404858464733!2d-65.4106297!3d-24.7700855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x941bc3d7c9590759%3A0x241c18ada1bffa66!2sIVOS%20-%20Revestimientos%20Alternativos!5e0!3m2!1sen!2smx!4v1743870234313!5m2!1sen!2smx";
 const ADDRESS = "Balcarce 1587, Salta Capital";
+const MAPS_URL = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+  `IVOS - Revestimientos Alternativos, ${ADDRESS}`
+)}`;
 
 const LINKS = [
   {
@@ -57,7 +60,7 @@ export const LinksPage = () => {
       />
       <div className="absolute inset-0 bg-white/90" />
 
-      <div className="relative z-10 flex w-full max-w-md flex-col items-center px-6 pb-8 pt-[4vh] lg:pt-[2vh]">
+      <div className="relative z-10 flex w-full max-w-md flex-col items-center px-6 pb-6 pt-[3vh] lg:pt-[1vh]">
         <Image
           src="/assets/images/logo-azulrojo.png"
           alt="IVOS"
@@ -97,12 +100,17 @@ export const LinksPage = () => {
               className="h-50 w-full"
             />
           </div>
-          <div className="flex items-center gap-2 font-medium text-brand-blue">
+          <Link
+            href={MAPS_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex underline items-center gap-2 font-medium text-brand-blue transition-colors duration-300 ease-out hover:text-brand-red"
+          >
             <svg width="16" height="19" viewBox="0 0 18 21" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M9 0c2.3869 0 4.6761.948211 6.364 2.63604C17.0518 4.32387 18 6.61305 18 9c0 3.074-1.676 5.59-3.442 7.395-.8823.8921-1.8451 1.7008-2.876 2.416l-.426.29-.2.133-.377.24-.336.205-.416.242c-.28237.1612-.60187.2459-.927.2459-.32513 0-.64463-.0847-.927-.2459l-.416-.242-.52-.32-.192-.125-.41-.273c-1.11217-.7525-2.1481-1.6119-3.093-2.566C1.676 14.589 0 12.074 0 9c0-2.38695.948211-4.67613 2.63604-6.36396C4.32387.948211 6.61305 0 9 0Zm0 6c-.39397 0-.78407.0776-1.14805.22836-.36398.15077-.69469.37174-.97327.65032-.27858.27858-.49955.60929-.65032.97327C6.0776 8.21593 6 8.60603 6 9c0 .39397.0776.78407.22836 1.1481.15077.3639.37174.6946.65032.9732.27858.2786.60929.4996.97327.6503C8.21593 11.9224 8.60603 12 9 12c.79565 0 1.5587-.3161 2.1213-.8787C11.6839 10.5587 12 9.79565 12 9c0-.79565-.3161-1.55871-.8787-2.12132C10.5587 6.31607 9.79565 6 9 6Z" fill="#F94E19" />
             </svg>
             <p>{ADDRESS}</p>
-          </div>
+          </Link>
         </div>
       </div>
     </main>
